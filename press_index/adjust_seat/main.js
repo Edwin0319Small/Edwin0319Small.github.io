daba37c224546cb567551d39a13b5767a21d4bdf
Edwin0319 <101723863+Edwin0319@users.noreply.github.com>
2024-11-02 19:16:53 +0800

    Add files via upload

--- press_index/adjust_seat/main.js
+++ press_index/adjust_seat/main.js
@@ -8,9 +8,17 @@ let vis = new Array(37).fill(false)
 const minimum = 1
 const maximum = 36
 var random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+const button = document.querySelector('button')
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let arr_assemble = []
 my_row.forEach((row)=>{
     // console.log(row)
-    let gender = -1;
+    /*
+    // 男一齊 女一齊
     for(let i = 0; i < 3;i++){
         while(vis[random]){
             random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
@@ -26,9 +34,143 @@ my_row.forEach((row)=>{
             e.currentTarget.classList.toggle('my-hidden')
         })
     }
+    */
+
+    for(let i = 0; i < 3;i++){  
+        row.children[i].addEventListener('click',(e)=>{
+            e.currentTarget.classList.toggle('font-red')
+        })
+    }
 })
 
-const button = document.querySelector('button')
+for(let k = 1; k <= my_row.length; k++){
+    let mod
+    if(k <= 9){
+        for(let i = 0; i < 3; i++){
+            while(vis[random]){
+                random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+            }
+            if(i == 0){
+                mod = Math.floor(Math.random() * 2)
+                arr_assemble.push([])
+            }
+            if(mod == 0){
+                if(i <= 1){
+                    while(vis[random] || arr_gender[random] == 0){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+                else{
+                    while(vis[random] || arr_gender[random] == 1){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+            }
+            else if(mod == 1){
+                if(i == 0){
+                    while(vis[random] || arr_gender[random] == 1){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+                else{
+                    while(vis[random] || arr_gender[random] == 0){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+            }
+            vis[random] = true
+            arr_assemble[k - 1].push(arr_students[random])
+        }
+    }
+    else{
+        for(let i = 0; i < 3; i++){
+            while(vis[random]){
+                random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+            }
+            if(i == 0){
+                mod = Math.floor(Math.random() * 2)
+                arr_assemble.push([])
+            }
+            if(mod == 0){
+                if(i <= 1){
+                    while(vis[random] || arr_gender[random] == 1){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+                else{
+                    while(vis[random] || arr_gender[random] == 0){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+            }
+            else if(mod == 1){
+                if(i == 0){
+                    while(vis[random] || arr_gender[random] == 0){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+                else{
+                    while(vis[random] || arr_gender[random] == 1){
+                        random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+                    }
+                }
+            }
+            vis[random] = true
+            arr_assemble[k - 1].push(arr_students[random])
+        }
+    }
+}
+console.log(arr_assemble)
+
+for(let i = 0; i < 3; i++){
+    my_row[0].children[i].innerHTML = arr_assemble[0][i]
+}
+for(let i = 0; i < 3; i++){
+    my_row[5].children[i].innerHTML = arr_assemble[1][i]
+}
+for(let i = 0; i < 3; i++){
+    my_row[6].children[i].innerHTML = arr_assemble[2][i]
+}
+for(let i = 0; i < 3; i++){
+    my_row[11].children[i].innerHTML = arr_assemble[3][i]
+}
+let kk = 4
+for(let k = 0; k < 12; k++){
+    if(k == 0 || k == 5 || k == 6 || k == 11) continue
+    for(let i = 0; i < 3; i++){
+        my_row[k].children[i].innerHTML = arr_assemble[kk][i]
+        // console.log(arr_assemble[kk][i], kk, i)
+        // console.log(my_row[k])
+    }
+    kk++
+}
+
+async function change_color_inside(row, delate){
+    for(let i = 0; i < 3; i++){
+        await sleep(delate)
+        if(button.getAttribute('mode') == "1"){
+            row.children[i].classList.add('font-red')
+        }
+        else{
+            row.children[i].classList.remove('font-red')
+        }
+    }
+}
+
+async function change_color_outside(delate){
+    for(let row = 0; row < my_row.length; row++){
+        await change_color_inside(my_row[row], delate)
+        await sleep(delate)
+    }
+}
 button.addEventListener('click', ()=>{
-    document.body.classList.toggle('my-gray')
+    console.log(button.getAttribute('mode'), typeof button.getAttribute('mode'))
+    change_color_outside(50)
+    if(button.getAttribute('mode') == "0"){
+        button.setAttribute('mode', "1")
+    }
+    else{
+        button.setAttribute('mode', "0")
+    }
 })
+
